cf0d0b78280ed2f5c4298731992d6272b2686473
LailaAli <laila@certedrive.com>
2018-10-11 15:48:12 -0400

    create result-summary component

--- src/app/purchase-details/purchase-details.component.ts
+++ src/app/purchase-details/purchase-details.component.ts
@@ -6,18 +6,15 @@ import { Component, OnInit, EventEmitter, Output, Input, } from '@angular/core';
   styleUrls: ['./purchase-details.component.scss']
 })
 export class PurchaseDetailsComponent implements OnInit {
+  // @Output() valueChange = new EventEmitter();
+  // counter = 0;
+
+  // valueChanged() { // You can give any function name
+  //     this.counter = this.counter + 1;
+  //     this.valueChange.emit(this.counter);
+  // }
 
   @Input() isHidden = false;
-
-
-
-  @Output() valueChange = new EventEmitter();
-  counter = 0;
-
-  valueChanged() { // You can give any function name
-      this.counter = this.counter + 1;
-      this.valueChange.emit(this.counter);
-  }
 
   ngOnInit() {}
 
